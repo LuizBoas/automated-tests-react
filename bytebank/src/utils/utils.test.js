a29d5e3753240b665851bc806ff32727a5fc7f92
LuizBoas <luiz.boas.ext@synchro.com.br>
2023-06-15 11:05:11 -0300

    add teste integracao

---
+++ bytebank/src/utils/utils.test.js
@@ -0,0 +1,24 @@
+import { calculaNovoSaldo } from './index';
+
+describe('Quando eu realizo uma transação', () => {
+  test('Que é um depósito, o saldo deve aumentar', () => {
+    const transacao = {
+      transacao: 'Depósito',
+      valor: 50,
+    };
+
+    const novoSaldo = calculaNovoSaldo(transacao, 100);
+
+    expect(novoSaldo).toBe(150);
+  });
+  test('Que é uma transferência, o saldo deve diminuir', () => {
+    const transacao = {
+      transacao: 'Transferência',
+      valor: 50,
+    };
+
+    const novoSaldo = calculaNovoSaldo(transacao, 100);
+
+    expect(novoSaldo).toBe(50);
+  });
+});
