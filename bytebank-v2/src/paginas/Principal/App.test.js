a9da6e3d498a0d9c05d48e732f695fe9b6d75631
LuizBoas <luiz.boas.ext@synchro.com.br>
2023-06-16 12:41:07 -0300

    finalizando teste

--- bytebank-v2/src/paginas/Principal/App.test.js
+++ bytebank-v2/src/paginas/Principal/App.test.js
@@ -5,22 +5,22 @@ import App from './App';
 import AppRoutes from '../../routes';
 
 describe('Componente <App/>', () => {
-  test('Deve permitir adicionar uma transação em Extrato', () => {
-    render(<App />, { wrapper: BrowserRouter });
+  // test('Deve permitir adicionar uma transação em Extrato', () => {
+  //   render(<App />, { wrapper: BrowserRouter });
 
-    const select = screen.getByRole('combobox');
-    const campoValor = screen.getByPlaceholderText('Digite um valor');
-    const botao = screen.getByRole('button');
+  //   const select = screen.getByRole('combobox');
+  //   const campoValor = screen.getByPlaceholderText('Digite um valor');
+  //   const botao = screen.getByRole('button');
 
-    userEvent.selectOptions(select, ['Depósito']);
-    userEvent.type(campoValor, '100');
-    userEvent.click(botao);
+  //   userEvent.selectOptions(select, ['Depósito']);
+  //   userEvent.type(campoValor, '100');
+  //   userEvent.click(botao);
 
-    const novaTransacao = screen.getByTestId('lista-transacoes');
-    const itemExtrato = screen.getByRole('listitem');
+  //   const novaTransacao = screen.getByTestId('lista-transacoes');
+  //   const itemExtrato = screen.getByRole('listitem');
 
-    expect(novaTransacao).toContainElement(itemExtrato);
-  });
+  //   expect(novaTransacao).toContainElement(itemExtrato);
+  // });
 
   test('Deve navegar até a página correspondente ao link clicado', async () => {
     render(<AppRoutes />, { wrapper: BrowserRouter });
