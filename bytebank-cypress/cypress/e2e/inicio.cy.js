121f70922ce65c8f70f2e1f71886f6e02388dfd0
LuizBoas <luiz.boas.ext@synchro.com.br>
2023-06-20 11:30:54 -0300

    add testes formulario

--- bytebank-cypress/cypress/e2e/inicio.cy.js
+++ bytebank-cypress/cypress/e2e/inicio.cy.js
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Pagina inicial', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000');
+  });
+  it('Deve renderizar o h1 o texto correto', () => {
+    cy.getByData('titulo-principal').contains(
+      'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
+    );
   });
 });
